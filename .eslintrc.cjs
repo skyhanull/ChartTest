ca102e9520e5456dd282d8318dae65bb5e4af55f
skyhanull <anky0425@naver.com>
2023-04-25 21:58:11 +0900

    feat: airbnb 플러그인 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,21 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh", "react", "@typescript-eslint", "prettier"],
-  rules: {
-    "react-refresh/only-export-components": "warn",
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-};
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
---
+++ src/apis/chartApi.tsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+// import { ITableList } from "../Types";
+
+const getChartInfo = async () => {
+  const res = await axios("mock_data.json");
+
+  if (res.status === 200) {
+    const { response } = res.data;
+    return response;
+    // return response as ITableList;
+  }
+  return null;
+};
+
+export default getChartInfo;
