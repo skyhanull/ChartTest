372b61e063352c4335ea06a726c99819f030525f
skyhanull <anky0425@naver.com>
2023-04-29 03:46:33 +0900

    feat: query설치 및 코드 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import './index.css'
+import App from './App'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
 )
